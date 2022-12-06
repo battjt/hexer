b17f96e552fc62dea283c627a17f1e1072bb7be2
Joe Batt <Joe@SolidDesign.net>
2022-12-06 14:57:51 -0500

    Single Arc

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,15 @@ fn main() {
     let application = App::default();
     let mut window = Window::default().with_label("Hexer").with_size(200, 100);
     let frame = Frame::default().with_size(100, 20).center_x(&window);
-    let  dec = Input::default()
+    let dec = Input::default()
         .with_label("dec")
         .size_of(&frame)
         .below_of(&frame, 0);
-    let  hex = Input::default()
+    let hex = Input::default()
         .with_label("hex")
         .size_of(&frame)
         .below_of(&dec, 0);
-    let  bin = Input::default()
+    let bin = Input::default()
         .with_label("bin")
         .size_of(&frame)
         .below_of(&hex, 0);
@@ -28,41 +28,33 @@ fn main() {
     window.end();
     window.show();
 
-    let d = Arc::new(Mutex::new(dec));
-    let h = Arc::new(Mutex::new(hex));
-    let b = Arc::new(Mutex::new(bin));
+    // pass fields as tuple to reduce number of locks (and subsequent copy and paste)
+    let fields = Arc::new(Mutex::new((dec, hex, bin)));
 
-    d.lock()
-        .unwrap()
-        .set_callback(create_cb(|i| i.value().parse().unwrap(), &d, &h, &b));
-
-    h.lock().unwrap().set_callback(create_cb(
+    let (mut dec, mut hex, mut bin) = fields.clone().lock().unwrap().to_owned();
+    dec.set_callback(create_cb(|i| i.value().parse().unwrap(), &fields));
+    hex.set_callback(create_cb(
         |i| u64::from_str_radix(i.value().as_str(), 16).unwrap(),
-        &d,
-        &h,
-        &b,
+        &fields,
     ));
+    bin.set_callback(create_cb(|i| i.value().parse().unwrap(), &fields));
 
     application.run();
 }
 
 fn create_cb(
-    var_name: fn(&Input) -> u64,
-    d: &Arc<Mutex<Input>>,
-    h: &Arc<Mutex<Input>>,
-    b: &Arc<Mutex<Input>>,
+    parse_fn: fn(&Input) -> u64,
+    fields: &Arc<Mutex<(Input, Input, Input)>>,
 ) -> impl FnMut(&mut Input) {
-    let d = d.clone();
-    let h = h.clone();
-    let b = b.clone();
-    move |e: &mut Input| {
-        let n = (var_name)(e);
-        update(n, &d, &h, &b);
+    let fields = fields.clone();
+    move |input: &mut Input| {
+        update(parse_fn(input), &fields);
     }
 }
 
-fn update(n: u64, d: &Arc<Mutex<Input>>, h: &Arc<Mutex<Input>>, b: &Arc<Mutex<Input>>) {
-    d.lock().unwrap().set_value(n.to_string().as_str());
-    h.lock().unwrap().set_value(&format!("{:X}", n));
-    b.lock().unwrap().set_value(&format!("{:b}", n));
+fn update(value: u64, fields: &Arc<Mutex<(Input, Input, Input)>>) {
+    let (mut dec, mut hex, mut bin) = fields.lock().unwrap().to_owned();
+    dec.set_value(value.to_string().as_str());
+    hex.set_value(&format!("{:X}", value));
+    bin.set_value(&format!("{:b}", value));
 }
